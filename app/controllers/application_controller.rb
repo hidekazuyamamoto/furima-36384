3fa095a7d95b23cc41c16bca107829b8f3d2ca76
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-04 20:19:31 +0900

    ユーザー登録実装修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
 private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :frist_name, :frist_name_kana, :last_name, :last_name_kana, :brithday])
+  def configure_permitted_parameters 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :first_name_kana, :last_name, :last_name_kana, :birthday])
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
