568b5a053fedf585769363b9b8612af1c5607afe
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-05 18:27:45 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,4 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
   belongs_to :category
@@ -8,12 +6,15 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :days
 
+  belongs_to :user
+  has_one_attached :image
+
   with_options presence: true do
     validates :name
     validates :explanation
     validates :price
   end
-  
+
   with_options numericality: { other_than: 1 } do
     validates :area_id 
     validates :category_id  
