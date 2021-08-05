779593e6e48f1f89b2f9a18508d1e78ec127b321
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-05 17:54:21 +0900

    activehashのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :statue
+  belongs_to :postage
+  belongs_to :days
+  
 end
