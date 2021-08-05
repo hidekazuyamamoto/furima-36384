890a4df107a5492b103580dae0da16770c3e819c
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-05 17:29:12 +0900

    マイグレーション修正

--- db/migrate/20210805080019_create_items.rb
+++ db/migrate/20210805080019_create_items.rb
@@ -3,10 +3,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
 
       t.timestamps
-      t.string :name, null:false
-      t.text :explanation, null:false
-      t.integer :price, null:false
-      t.user :references, null:false, foreign_key:true
+      t.string :name, null: false
+      t.text :explanation, null: false
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
     end
   end
 end
