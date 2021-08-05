6f194b811d2ba00d86f673c69330a6de18ca6bdf
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-06 07:17:33 +0900

    ログインしないと出品できない実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :new
   def index
   end
   def new
@@ -16,5 +17,11 @@ class ItemsController < ApplicationController
   private
   def item_paeams
     params.required(:item).permit(:image, :name, :explanation, :price, :area_id, :category_id, :status_id, :postage_id, :days_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
