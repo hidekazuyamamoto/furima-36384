8885a59484f87624ec7b5962948be1394fba3334
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-06 07:10:24 +0900

    new,createアクション追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,19 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
   end
   def create
+    @item = Item.create(item_paeams)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_paeams
+    params.required(:item).permit(:image, :name, :explanation, :price, :area_id, :category_id, :status_id, :postage_id, :days_id).merge(user_id: current_user.id)
   end
 end
