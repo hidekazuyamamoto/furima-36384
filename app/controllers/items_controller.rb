e4f00d00d1cf1319a8b60f2792d85beeaef39106
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-06 14:12:03 +0900

    未ログイン状態では出品できない実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,13 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: :new
   def index
+    @item = Item.includes(:user).order('create_at DESC')
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.create(item_paeams)
     if @item.save
@@ -15,13 +18,15 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_paeams
-    params.required(:item).permit(:image, :name, :explanation, :price, :area_id, :category_id, :status_id, :postage_id, :days_id).merge(user_id: current_user.id)
+    params.required(:item).permit(:image, :name, :explanation, :price, :area_id, :category_id, :status_id, :postage_id,
+                                  :days_id).merge(user_id: current_user.id)
   end
 
   def move_to_index
     unless user_signed_in?
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
   end
 end
