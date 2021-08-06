078403c00044607becc91d13cc6d37f70fe0cf5d
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-06 11:19:40 +0900

    factorybot,faker設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                 {Faker::Name.name}
+    explanation          {"テスト"}
+    price                {Faker::Number.bitween(from: 300, to: 9999999)}
+    area_id              {Faker::Number.between(from: 2, to: 48)}
+    category_id          {Faker::Number.between(from: 2, to: 11)}
+    status_id            {Faker::Number.between(from: 2, to: 7)}
+    postage_id           {Faker::Number.between(from: 2, to: 3)}
+    days_id              {Faker::Number.between(from: 2, to: 4)}
+    association :user
   end
 end
