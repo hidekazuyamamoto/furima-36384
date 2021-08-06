2e583eaa6b7d38f9c47d56e01c236af24b1fd02e
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-06 13:59:26 +0900

    factory-botに画像を添付

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :item do
     name                 {Faker::Name.name}
-    image                {Faker::Lorem.sentence}
     explanation          {"テスト"}
     price                {Faker::Number.between(from: 300, to: 9999999)}
     area_id              {Faker::Number.between(from: 2, to: 48)}
@@ -10,5 +9,8 @@ FactoryBot.define do
     postage_id           {Faker::Number.between(from: 2, to: 3)}
     days_id              {Faker::Number.between(from: 2, to: 4)}
     association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
