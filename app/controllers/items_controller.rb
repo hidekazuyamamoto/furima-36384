05586ef7e9084f44840451affb2d163cf518ac2d
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-09 16:11:55 +0900

    ログインが異なる場合の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: [:edit]
   def index
      @items = Item.includes(:user).order("created_at DESC")
   end
@@ -36,6 +37,12 @@ class ItemsController < ApplicationController
   def item_params
     params.required(:item).permit(:image, :name, :explanation, :price, :area_id, :category_id, :status_id, :postage_id,
                                   :days_id).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    item = Item.find(params[:id])
+    unless current_user.id == item.user.id
+      redirect_to root_path
+    end
   end
 
 end
