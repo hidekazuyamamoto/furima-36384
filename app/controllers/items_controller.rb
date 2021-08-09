a6256f5d6ef5ae960d578239aa26d2544ba8a3b8
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-09 16:50:20 +0900

    商品情報編集機能修正　（8/9 16:55）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:destroy, :show, :index]
   before_action :set_item, only:[:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
   def index
      @items = Item.includes(:user).order("created_at DESC")
   end
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     if @item.update(item_params)
       redirect_to item_path(params[:id])
@@ -39,14 +39,13 @@ class ItemsController < ApplicationController
     params.required(:item).permit(:image, :name, :explanation, :price, :area_id, :category_id, :status_id, :postage_id,
                                   :days_id).merge(user_id: current_user.id)
   end
-  def move_to_index
-    item = Item.find(params[:id])
-    unless current_user.id == item.user.id
-      redirect_to root_path
-    end
-  end
   def set_item
     @item = Item.find(params[:id])
+  end
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
 end
