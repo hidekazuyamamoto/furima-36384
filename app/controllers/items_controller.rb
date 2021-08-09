ba4533e4355149a1fc71e0a3328ddf5abe676837
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-09 17:00:06 +0900

    商品情報編集機能修正（8/9 17:00）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:destroy, :show, :index]
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_item, only:[:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
   def index
