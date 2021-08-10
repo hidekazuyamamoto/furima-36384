8cea7a41d9fb2841789403b0b30bf90340b948ed
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-10 14:28:14 +0900

    モデル作成マイグレーション済

---
+++ db/migrate/20210810052343_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+
+      t.timestamps
+      t.references :purchased, null:false, foreign_key:true
+      t.string :postal_code, null:false
+      t.string :city, null:false
+      t.string :address, null:false
+      t.string :building_name
+      t.string :telephone_number, null:false
+      t.integer :area_id, null:false
+    end
+  end
+end
