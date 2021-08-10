4a39144620d42e1605604d1ba809bf59e823246e
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-10 17:28:39 +0900

    ストロングパラメーター実装（仮）

--- app/controllers/purchaseds_controller.rb
+++ app/controllers/purchaseds_controller.rb
@@ -8,6 +8,16 @@ class PurchasedsController < ApplicationController
   end
   
   def create
+    @purchased = Purchased.create(params_purchased)
+    Order.create(params_order)
+    redirect_to root_path
   end
 
+  private
+  def params_purchased
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+  def params_order
+    params.permit(:postal_code, :area_id, :city, :address, :building_name, :telephone_number).merge(purchased_id: @purchased.id)
+  end
 end
