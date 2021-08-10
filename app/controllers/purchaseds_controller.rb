46c2d932496c41ecfacf7283fd2048924d47a83c
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-10 17:52:10 +0900

    コントローラー編集

--- app/controllers/purchaseds_controller.rb
+++ app/controllers/purchaseds_controller.rb
@@ -5,19 +5,20 @@ class PurchasedsController < ApplicationController
   end
 
   def new
+    @purchased_order = PurchasedOrde.new
   end
   
   def create
-    @purchased = Purchased.create(params_purchased)
-    Order.create(params_order)
-    redirect_to root_path
+    @params_purchased = PurchasedOrde.create(params_purchased)
+    if @params_purchased.valid?
+      @params_purchased.save
+      redirect_to root.path
+    else
+      render :new
   end
 
   private
   def params_purchased
-    params.permit(:item_id).merge(user_id: current_user.id)
-  end
-  def params_order
-    params.permit(:postal_code, :area_id, :city, :address, :building_name, :telephone_number).merge(purchased_id: @purchased.id)
+    params.require(:PurchasedOrde).permit(:postal_code, :city. :address, :building_name, :telephone_number, :area_id).merge(user_id: current_user.id)
   end
 end
