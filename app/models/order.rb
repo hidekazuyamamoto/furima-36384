d8a43c97d061626e522d1033470161318b0846c2
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-10 17:45:31 +0900

    purchased_orderモデル作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,5 @@ class Order < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
   belongs_to :purchased
-  with_options presence: true do
-    validates :postal_code
-    validates :city
-    validates :address
-    validates :telephone_number
-    with_options numericality:{ other_than: 0} do
-      validates :area_id
-    end
-  end
+  
 end
---
+++ app/models/purchased_order.rb
@@ -0,0 +1,17 @@
+class PurchasedOrder
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :city, :address, :building_name, :telephone_number, :area_id
+  with_options presence: true do
+    validates :postal_code
+    validates :city
+    validates :address
+    validates :telephone_number
+    with_options numericality:{ other_than: 0} do
+      validates :area_id
+    end
+  end
+  def save
+    purchased = Purchased.create(user_id: user_id, item_id: item_id)
+    Order.create(purchased_id: purchased.id, postal_code: postal_code, city: city, address: address, building_name: building_name, telephone_number: telephone_number, area_id: area_id)
+  end
+end
