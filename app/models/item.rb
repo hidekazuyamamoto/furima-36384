353c89addffc04d624d8c3e13726281776f4cea1
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-15 11:55:40 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :days
 
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_one_attached :image, dependent: :destroy
   has_one :purchased
 
